43544e108444f04e23173fe9d0cb1c5651759b68
MathCT <mathieu.chatrain@gmail.com>
2025-09-24 16:36:52 +0200

    commit

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Articles;
+use App\Form\ArticleType;
+use App\Repository\ArticlesRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/articles')]
+final class ArticlesController extends AbstractController
+{
+    #[Route('/', name: 'articles', methods:['GET'])]
+    public function index(ArticlesRepository $ArticlesRepo): Response
+    {
+
+        $articles = $ArticlesRepo->findAll();
+
+       
+        return $this->render('articles/index.html.twig', [
+            'articles' => $articles
+        ]);
+    }
+
+    #[Route('/read/{id}', name: 'article', methods:['GET'])]
+    public function read(ArticlesRepository $ArticlesRepo, int $id, Articles $article): Response
+    {
+    
+        return $this->render('articles/read.html.twig', [
+            'article' => $article
+        ]);
+    }
+
+
+    #[Route('/new', name:'article_new', methods:['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        // je crée une instance de Pokemon
+        $article = new Articles();
+
+        // permet de créer le form a partir du Type (les inputs) et de l'instance(les verifs des champs)
+        $form = $this->createForm(ArticleType::class, $article);
+
+        //recupere la requete en post ou get de ce formulaire (et seulement ce formulaire)
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            
+            $em->persist($article);  //equivalent du prepare
+            $em->flush(); //equivalent du execute
+
+            // revenir sur la page des pokemon (la route qui a le name 'pokemons')
+            return $this->redirectToRoute('articles');
+        }
+
+        return $this->render('articles/new.html.twig', [
+            'form' => $form
+        ]);
+    
+    }
+
+    #[Route('/delete/{id}', name:'article_delete', methods:['POST'])]
+    public function delete(int $id, Request $request, Articles $article, EntityManagerInterface $em): Response //Pokemon $pokemon permet de se passer de $pokemon = $PokemonRepo->findOneBy(["id" => $id]);
+    {
+        if($this->isCsrfTokenValid('delete'.$id, $request->request->get('_token')))
+        {
+            $em->remove($article);
+            $em->flush();
+            $this->addFlash('success', "votre article a été supprimé");
+            return $this->redirectToRoute('articles');
+        }
+
+        else {
+            $this->addFlash('error', "echec de la suppression");
+            return $this->redirectToRoute('articles');
+        }
+      
+    }
+
+    #[Route('/edit/{id}', name:'article_edit', methods:['GET', 'POST'])]
+    public function edit(int $id, Request $request, Articles $article, EntityManagerInterface $em): Response //Pokemon $pokemon permet de se passer de $pokemon = $PokemonRepo->findOneBy(["id" => $id]);
+    {
+        $date = new \DateTime('now', new \DateTimeZone('Europe/Paris'));
+        $article->setUpdatedAt($date);
+        $form = $this->createForm(ArticleType::class, $article);
+
+        //recupere la requete en post ou get de ce formulaire (et seulement ce formulaire)
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+              
+            $em->flush(); 
+            $this->addFlash('success', "votre pokemon a été modifié");
+            // revenir sur la page des pokemon (la route qui a le name 'pokemons')
+            return $this->redirectToRoute('articles');
+        }
+
+         return $this->render('articles/edit.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+
+}
---
+++ src/Entity/Articles.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticlesRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ArticlesRepository::class)]
+class Articles
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    #[Assert\Choice(choices: [1, 2, 3], message: 'Le statut doit être 1, 2 ou 3.')]
+    private ?int $status = null;
+
+    #[ORM\Column]
+    private ?\DateTime $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTime $updatedAt = null;
+
+
+
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime('now', new \DateTimeZone('Europe/Paris')); // Met automatiquement la date du jour
+       
+    }
+
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTime $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+}
